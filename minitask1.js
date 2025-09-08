f496b4694049f6fa314d5870e136dcfc34422fbd
ghifar1327 <givartorreto@gmail.com>
2025-09-08 10:15:45 +0700

    Add files via upload

---
+++ minitask1.js
@@ -0,0 +1,29 @@
+const motor = ['honda', 'yamaha', 'suzuki'];
+let cari = 'suzuki'
+
+let index = -1;
+for (let i = 0; i < motor.length; i++) {
+    if (motor[i] === cari) {
+        index = i;
+    }
+}
+
+if (index !== -1) {
+    console.log('merek motor tersedia');
+} else {
+    console.log('merek motor tidak tersedia');
+}
+
+// mngunakan indexOf
+
+
+const hotel = ['mercure', 'swiss-bellin', 'oyo'];
+let search = 'marcure';
+
+let index1 = hotel.indexOf(search);
+
+if (index1 !== -1) {
+    console.log('hotel ada');
+} else {
+    console.log('hotel tidak ada');
+}
